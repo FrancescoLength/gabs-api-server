97984c9400e8ee35ba12c6aaed70e38c8cd106cc
FrancescoLength <france.schino@live.it>
2026-05-15 14:43:39 +0100

    chore: increase default rate limits for API requests

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ swagger = Swagger(app)
 limiter = Limiter(
     get_remote_address,
     app=app,
-    default_limits=["200 per day", "50 per hour"],
+    default_limits=["5000 per day", "500 per hour"],
     storage_uri="memory://",
 )
 app.start_time = datetime.now()
